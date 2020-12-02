18d4a429dbea4dd6edf8c1171f19124779cc60a9
Ali Sheikh <ali.sheikh@flexport.com>
2020-12-01 16:48:37 -0800

    Updates

---
+++ precache-manifest.ef1c58a0bf5aab5d48f57ce3a5dd8bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998567341376722f81c3b1d0055cd8fd",
+    "url": "/Sheikh-A/CRA_insta/index.html"
+  },
+  {
+    "revision": "916ad3d7e6babf4fb2e9",
+    "url": "/Sheikh-A/CRA_insta/static/css/main.0336fdac.chunk.css"
+  },
+  {
+    "revision": "2c0ed6a8759ecc335a5c",
+    "url": "/Sheikh-A/CRA_insta/static/js/2.25e3912d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sheikh-A/CRA_insta/static/js/2.25e3912d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "916ad3d7e6babf4fb2e9",
+    "url": "/Sheikh-A/CRA_insta/static/js/main.28f26bf3.chunk.js"
+  },
+  {
+    "revision": "2f13f21ad0e8b4efd27e",
+    "url": "/Sheikh-A/CRA_insta/static/js/runtime-main.099c5411.js"
+  },
+  {
+    "revision": "e042e2bbcc453a57f7787838be9402ba",
+    "url": "/Sheikh-A/CRA_insta/static/media/kenz.e042e2bb.png"
+  }
+]);
